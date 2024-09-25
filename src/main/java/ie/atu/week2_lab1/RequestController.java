a09b37ce45168eab21a28ce1235b4388458b81e6
Shine Sujith <G00414454@atu.ie>
2024-09-25 15:41:14 +0100

    added a method to add, subtract, multiply, and divide two numbers

--- src/main/java/ie/atu/week2_lab1/RequestController.java
+++ src/main/java/ie/atu/week2_lab1/RequestController.java
@@ -8,19 +8,46 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class RequestController {
 
-    @GetMapping("/sayHello")
+    @GetMapping("/hello")
     public String sayHello() {
         return "Hello";
     }
 
     @GetMapping("/greet/{name}")
     public String greetByName(@PathVariable String name) {
-        return "hello " + name;
+        return "Hello " + name;
     }
 
     @GetMapping("/details")
     public String details(@RequestParam String name, @RequestParam int age) {
         return "Name: "+ name + "\tage: " + age;
+    }
+
+    @GetMapping("/calculate")
+    public String calculate(@RequestParam int num1, @RequestParam int num2, @RequestParam String operation) {
+        float ans = 0;
+        switch (operation) {
+            case "add":
+                ans = num1 + num2;
+                break;
+            case "subtract":
+                ans = num1 - num2;
+                break;
+            case "multiply":
+                ans = num1 * num2;
+                break;
+            case "divide":
+                if(num2  == 0) {
+                    return "can't divide by 0";
+                }
+                else {
+                    ans = (float)num1/num2;
+                }
+                break;
+            default:
+                break;
+        }
+        return Float.toString(ans);
     }
 
 }
