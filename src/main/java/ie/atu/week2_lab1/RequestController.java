a3d887be5815640819f03ff74bea89c53be38954
Shine Sujith <G00414454@atu.ie>
2024-09-25 14:46:53 +0100

    added a method to say hello

--- src/main/java/ie/atu/week2_lab1/RequestController.java
+++ src/main/java/ie/atu/week2_lab1/RequestController.java
@@ -1,4 +1,14 @@
 package ie.atu.week2_lab1;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class RequestController {
+
+    @GetMapping("/sayHello")
+    public String sayHello() {
+        return "Hello";
+    }
+
 }
