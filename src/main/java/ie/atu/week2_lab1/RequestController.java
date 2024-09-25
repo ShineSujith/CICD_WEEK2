f7f2d18ec2ff12b67f68c5eba665362ffc3258a5
Shine Sujith <G00414454@atu.ie>
2024-09-25 14:52:26 +0100

    added a method to greet by name

--- src/main/java/ie/atu/week2_lab1/RequestController.java
+++ src/main/java/ie/atu/week2_lab1/RequestController.java
@@ -1,6 +1,7 @@
 package ie.atu.week2_lab1;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,6 +10,11 @@ public class RequestController {
     @GetMapping("/sayHello")
     public String sayHello() {
         return "Hello";
+    }
+
+    @GetMapping("/greet/{name}")
+    public String greetByName(@PathVariable String name) {
+        return "hello " + name;
     }
 
 }
