77cf7b850a31c828db693839322e0dbddd2a090b
Shine Sujith <G00414454@atu.ie>
2024-09-25 14:33:11 +0100

    new project

---
+++ src/main/java/ie/atu/week2_lab1/RequestController.java
@@ -0,0 +1,4 @@
+package ie.atu.week2_lab1;
+
+public class RequestController {
+}
---
+++ src/main/java/ie/atu/week2_lab1/Week2Lab1Application.java
@@ -0,0 +1,11 @@
+package ie.atu.week2_lab1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week2Lab1Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Week2Lab1Application.class, args);
+	}
+}
