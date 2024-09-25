63256b7f67b3ca10c0913653c8a8e4934f3f7495
Shine Sujith <G00414454@atu.ie>
2024-09-25 15:01:15 +0100

    added a method to display name and age using RequestParam

--- src/main/java/ie/atu/week2_lab1/RequestController.java
+++ src/main/java/ie/atu/week2_lab1/RequestController.java
@@ -2,6 +2,7 @@ package ie.atu.week2_lab1;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,6 +16,11 @@ public class RequestController {
     @GetMapping("/greet/{name}")
     public String greetByName(@PathVariable String name) {
         return "hello " + name;
+    }
+
+    @GetMapping("/details")
+    public String details(@RequestParam String name, @RequestParam int age) {
+        return "Name: "+ name + "\tage: " + age;
     }
 
 }
